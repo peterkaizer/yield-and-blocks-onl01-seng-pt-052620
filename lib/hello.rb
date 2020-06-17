820782ad668d6cdb56ca9e018dabd8a78bc86dda
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-17 00:36:19 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,3 +1,9 @@
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
+
 def hello_t(array)
   i = 0
   while i < array.length
@@ -5,9 +11,3 @@ def hello_t(array)
     i = i + 1 
   end
 end 
-
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
